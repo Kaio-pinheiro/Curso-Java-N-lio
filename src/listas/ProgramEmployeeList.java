b06c68311b953c9264bc30fd507f779aadcf533f
kaio <kaio.devfull@gmail.com>
2024-10-21 16:37:36 -0300

    update

--- src/listas/ProgramEmployeeList.java
+++ src/listas/ProgramEmployeeList.java
@@ -39,15 +39,15 @@ public class ProgramEmployeeList {
 		System.out.print("Enter the employeee id that will have salary increase : ");
 		int idsalary = leia.nextInt();
 		// Procurando a posição do idsalary(funcionário que vai receber o aumento) dentro da lista
-		// se não encontrar vai retornar null e se retorna pergunto a porcentagem, a ser aumentada
-		// ai vou acessar na lista o funcionario que esta na posicao que eu encontrei *pos* e vou chamar a função pra aumentar o salario dele
+		// se não encontrar vai retornar null e se retorna pergunto a porcentagem a ser aumentada
+		// ai vou acessar na lista o funcionário que está na posição que eu encontrei *pos* e vou chamar a função pra aumentar o salário dele
 		Integer pos = position(list, idsalary); 
 		if(pos == null) {
 			System.out.println("This is id does not exist!");
 		}else {
 			System.out.print("Enter the percentage: ");
 			double percent = leia.nextDouble();
-			//get(pos) acessa o funcionario que esta na posição pos e chama a função de aumentar o salario
+			//get(pos) acessa o funcionário que está na posição pos e chama a função de aumentar o salario
 			list.get(pos).increaseSalary(percent);
 		}
 		System.out.println();
@@ -60,9 +60,9 @@ public class ProgramEmployeeList {
 	}
 	
 	// Função pra procurar a posição do elemento na lista
-	// vai percorrer a lista* list.size()* o tamanho dela, se o elemento da posição i *list.get(i)* .getId() 
-	//ou seja peguei o id dele, se esse id for igual o id que forneci como argumento encontrou e a posiça~odele é a posição de i
-	//;; usando int retorna -1 usando Integer retorna null
+	// vai percorrer a lista *list.size()* o tamanho dela, se o elemento da posição i *list.get(i)* .getId() 
+	// peguei o id dele, se esse id for igual o id que forneci como argumento, encontrou e a posição dele é a posição i
+	// usando int retorna -1 usando Integer retorna null
 	public static Integer position(List<EmployeeList> list, int id) {
 		for(int i = 0; i < list.size(); i++) {
 			if (list.get(i).getId() == id) {
