84657dbb889e2e226c8e3cb92d0412270e45185f
kaio <kaio.devfull@gmail.com>
2024-10-21 17:12:56 -0300

    update

--- src/matrizes/Program.java
+++ src/matrizes/Program.java
@@ -11,19 +11,19 @@ public class Program {
 		int n = leia.nextInt();
 		int[][] mat = new int[n][n];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < n; j++) {
 				mat[i][j] = leia.nextInt();
 			}
 		}
 
 		System.out.println("Main diagonal: ");
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < mat.length; i++) {
 			System.out.print(mat[i][i] + " ");
 		}
 		System.out.println();
 		int count = 0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < n; j++) {
 				if (mat[i][j] < 0) {
 					count++;
