7f24c62e7d2c4e0e0da53f369fa672adb1381986
kaio <kaio.devfull@gmail.com>
2024-10-22 12:51:41 -0300

    update

--- src/estrutura_condicional/Exercicio03.java
+++ src/estrutura_condicional/Exercicio03.java
@@ -3,22 +3,22 @@ package estrutura_condicional;
 import java.util.Scanner;
 
 public class Exercicio03 {
-	
+
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		
+
 		System.out.println("Digite um número para A:");
 		int A = leia.nextInt();
 		System.out.println("Digite um número para B:");
 		int B = leia.nextInt();
-		
-		if ( A % B == 0 || B % A == 0) {
+
+		if (A % B == 0 || B % A == 0) {
 			System.out.println("São múltiplos");
 		} else {
 			System.out.println("Não são múltiplos");
 		}
-		
+
 		leia.close();
-		
+
 	}
 }
