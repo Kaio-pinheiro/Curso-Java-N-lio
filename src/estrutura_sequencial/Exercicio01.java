1ee1e6d5fd03c0524b262aac94ce2f179ced0c86
kaio <kaio.devfull@gmail.com>
2024-10-23 09:34:25 -0300

    update

--- src/estrutura_sequencial/Exercicio01.java
+++ src/estrutura_sequencial/Exercicio01.java
@@ -10,19 +10,18 @@ public class Exercicio01 {
 		 * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a
 		 * soma desses números com uma mensagem explicativa, conforme exemplos.
 		 */
-		
+
 		Scanner leia = new Scanner(System.in);
 		System.out.println("Digite o primeiro número:");
 		int primeiro = leia.nextInt();
 		System.out.println("Digite o segundo número:");
 		int segundo = leia.nextInt();
-		
+
 		int soma = primeiro + segundo;
 		System.out.println("SOMA = " + soma);
-		
-		
+
 		leia.close();
-		
+
 	}
 
 }
