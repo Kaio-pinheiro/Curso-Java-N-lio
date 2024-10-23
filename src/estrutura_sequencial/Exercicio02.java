d79e7a9e7be9e2b8f26d14b7a6c1455bc7d50107
kaio <kaio.devfull@gmail.com>
2024-10-23 09:34:37 -0300

    update

--- src/estrutura_sequencial/Exercicio02.java
+++ src/estrutura_sequencial/Exercicio02.java
@@ -6,18 +6,19 @@ import java.util.Scanner;
 public class Exercicio02 {
 
 	public static void main(String[] args) {
-		// Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos
+		// Faça um programa para ler o valor do raio de um círculo, e depois mostrar o
+		// valor da área deste círculo com quatro casas decimais conforme exemplos
 		// Fórmula da área: area = π . raio2
 		// Considere o valor de π = 3.14159
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner leia = new Scanner(System.in);
 		System.out.println("Digite o valor do raio:");
 		double raio = leia.nextDouble();
-		
+
 		double area = 3.14159 * (raio * raio);
 		System.out.printf("A=%.4f%n", area);
-		
+
 		leia.close();
 	}
 
