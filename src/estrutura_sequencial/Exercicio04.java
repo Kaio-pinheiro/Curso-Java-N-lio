296d643a91985576adc07ba6f8a56a99182805ec
kaio <kaio.devfull@gmail.com>
2024-10-23 09:35:05 -0300

    update

--- src/estrutura_sequencial/Exercicio04.java
+++ src/estrutura_sequencial/Exercicio04.java
@@ -14,21 +14,20 @@ public class Exercicio04 {
 		 */
 		Locale.setDefault(Locale.US);
 		Scanner leia = new Scanner(System.in);
-		
+
 		System.out.println("Digite o número do funcionario?!");
 		int numero = leia.nextInt();
 		System.out.println("Quantas horas você trabalhou?");
 		int horasTrabalhadas = leia.nextInt();
 		System.out.println("Quanto você recebe por hora?");
 		double salarioPorHora = leia.nextDouble();
-		
+
 		double salario = horasTrabalhadas * salarioPorHora;
 		System.out.println("NUMBER = " + numero);
 		System.out.printf("SALARY = U$ %.2f%n", salario);
-		
+
 		leia.close();
-		
-		
+
 	}
 
 }
--- src/estrutura_sequencial/Exercicio05.java
+++ src/estrutura_sequencial/Exercicio05.java
@@ -6,13 +6,12 @@ import java.util.Scanner;
 public class Exercicio05 {
 
 	public static void main(String[] args) {
-		
 
 		// Fazer um programa para ler o código de uma peça 1, o número de peças 1, o
 		// valor unitário de cada peça 1, o
 		// código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2.
 		// Calcule e mostre o valor a ser pago.
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner leia = new Scanner(System.in);
 		System.out.println("Digite o código da peça 1:");
@@ -21,23 +20,23 @@ public class Exercicio05 {
 		int numeroPecas1 = leia.nextInt();
 		System.out.println("Digite o valor unitário de cada peça 1:");
 		double valorPeca1 = leia.nextDouble();
-		
+
 		System.out.println("Digite o código da peça 2:");
 		int codigoPeca2 = leia.nextInt();
 		System.out.println("Digite o número de peças 2:");
 		int numeroPecas2 = leia.nextInt();
 		System.out.println("Digite o valor unitário de cada peça 2:");
 		double valorPeca2 = leia.nextDouble();
-		
+
 		double total = (numeroPecas1 * valorPeca1) + (numeroPecas2 * valorPeca2);
-		
-		System.out.printf("VALOR A PAGAR: R$ %.2f%n", total );
-		
+
+		System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
+
 		// Só pra usar eles
-		System.out.println(codigoPeca1 + codigoPeca2 );
-		
+		System.out.println(codigoPeca1 + codigoPeca2);
+
 		leia.close();
-		
+
 	}
 
 }
