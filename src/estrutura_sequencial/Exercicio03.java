58b7ac66764c2b6d2500810010f8517067a339a2
kaio <kaio.devfull@gmail.com>
2024-10-23 09:34:51 -0300

    update

--- src/estrutura_sequencial/Exercicio03.java
+++ src/estrutura_sequencial/Exercicio03.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Exercicio03 {
 
 	public static void main(String[] args) {
-		// Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
-		// de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
-		
+		// Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir,
+		// calcule e mostre a diferença do produto
+		// de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C *
+		// D).
 
-		Scanner leia= new Scanner(System.in);
-		
+		Scanner leia = new Scanner(System.in);
+
 		System.out.println("Digite o primeiro valor?");
 		int A = leia.nextInt();
 		System.out.println("Digite o segundo valor?");
@@ -19,10 +20,10 @@ public class Exercicio03 {
 		int C = leia.nextInt();
 		System.out.println("Digite o quarto valor?");
 		int D = leia.nextInt();
-		
+
 		int soma = (A * B - C * D);
 		System.out.println("DIFERENÇA = " + soma);
-		
+
 		leia.close();
 	}
 
