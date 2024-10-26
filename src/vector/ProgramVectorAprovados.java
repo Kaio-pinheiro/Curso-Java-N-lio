4f4a1bed3aafdd0d6602a86125b18cf17d7f6643
kaio <kaio.devfull@gmail.com>
2024-10-25 21:32:59 -0300

    update

--- src/vector/ProgramVectorAprovados.java
+++ src/vector/ProgramVectorAprovados.java
@@ -12,33 +12,29 @@ public class ProgramVectorAprovados {
 
 		System.out.print("Quantos alunos serão digitados? ");
 		int n = sc.nextInt();
-		
+
 		String[] nomes = new String[n];
 		double[] nota1Trimestre = new double[n];
 		double[] nota2Trimestre = new double[n];
-		
+
 		for (int i = 0; i < n; i++) {
 			System.out.println("Digite nome, primeira e segunda nota do " + (i + 1) + "º aluno: ");
 			sc.nextLine();
 			nomes[i] = sc.nextLine();
 			nota1Trimestre[i] = sc.nextDouble();
 			nota2Trimestre[i] = sc.nextDouble();
-			
+
 		}
-		
+
 		System.out.println("Alunos aprovados: ");
 		double media;
-		for(int i = 0; i<n; i++) {
-		media = (nota1Trimestre[i] + nota2Trimestre[i]) / 2;
-			if(media >= 6.0) {
+		for (int i = 0; i < n; i++) {
+			media = (nota1Trimestre[i] + nota2Trimestre[i]) / 2;
+			if (media >= 6.0) {
 				System.out.println(nomes[i]);
 			}
 		}
-		
-		
-		
-		
-		
+
 		sc.close();
 	}
 
